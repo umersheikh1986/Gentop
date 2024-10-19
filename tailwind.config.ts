2aed7c9a80dea718839e696877a81864889db138
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2024-10-19 19:17:32 +0500

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        san: "Orbitron, sans-serif",
+      },
+      keyframes: {
+        slideAndFlip: {
+          "0%, 100%": {
+            transform: " translateY(-50)  translateY(0) translateY(-50)",
+          },
+          "50%": { transform: "translateX(0px) rotateX(180deg)" },
+        },
+        moveUpDown: {
+          "0%, 100%": {
+            transform: "translateY(0)   translateY(-100) rotateY(0deg)",
+          }, // Initial and final position
+          "50%": {
+            transform: "translateY(1px) translateY(0px) rotateY(180deg)  ",
+          }, // Moves down 20px
+        },
+      },
+      screens: {
+        xsm: "330px",
+        xsmm: "425px",
+        // => @media (min-width: 992px) { ... }
+      },
+      animation: {
+        slideAndFlip: "slideAndFlip 3s ease-in-out infinite",
+        moveUpDown: "moveUpDown 3s ease-in-out infinite", // 3-second loop
       },
     },
   },
