c7a478f8e8bdad1c8e9d1623df6b446ca966e465
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2024-11-01 15:43:30 +0500

    Update page.tsx

--- src/app/trade/page.tsx
+++ src/app/trade/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Web3Modal from "web3modal";
+import { ethers } from "ethers";
 import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
 import { WalletConnect } from "@web3-react/walletconnect";
 ("@web3-react/walletconnect");
-import { ethers } from "ethers";
 const usdtABI = [
   {
     inputs: [{ internalType: "address", name: "_owner", type: "address" }],
@@ -585,6 +585,11 @@ const preSaleABI = [
   },
 ];
 
+const usdtAddress = "0xB3e6AE901b927F76C6E8b24a0c5B2a6511f5067F"; // Replace with Contract 1 address
+const gentopAddress = "0xa5F286f59869E6090Ee1Fa36b9B628FFF9e98F2e"; // Replace with Contract 2 address
+const stakingContractAddress = "0x416B3FC8805a7A3D3F4e77B78561261DC4F0770C"; // Replace with Contract 3 address
+const preSaleContractAddress = "0x438E9efB39fC6903226809272e155baCdAB34EfF"; // Replace with Contract 4 address
+
 const providerOptions = {
   coinbasewallet: {
     package: CoinbaseWalletSDK,
@@ -593,42 +598,20 @@ const providerOptions = {
       infuraId: "https://rpc.testnet.fantom.network",
     },
   },
-  walletconnect: {
-    package: WalletConnect,
-    options: {
-      rpc: {
-        4002: "https://rpc.testnet.fantom.network", // Chain ID for Fantom Testnet
-      },
-      network: "fantom testnet", // Optional
-      qrcode: true, // Enable QR code for mobile wallets
-    },
-  },
 };
-const usdtAddress = "0xB3e6AE901b927F76C6E8b24a0c5B2a6511f5067F"; // Replace with Contract 1 address
-const gentopAddress = "0xa5F286f59869E6090Ee1Fa36b9B628FFF9e98F2e"; // Replace with Contract 2 address
-const stakingContractAddress = "0x416B3FC8805a7A3D3F4e77B78561261DC4F0770C"; // Replace with Contract 3 address
-const preSaleContractAddress = "0x438E9efB39fC6903226809272e155baCdAB34EfF"; // Replace with Contract 4 address
-
-let theme = {
-   background: "#202020", // Modal background color
-   main: "#ffffff", // Main text color
-   secondary: "#a0a0a0", // Secondary text color
-   border: "1px solid #ffffff", // Border style for modal
-   hover: "#f0f0f0", // Hover state color
- };
 
 export default function Trade() {
   const [selectedOption, setSelectedOption] = useState("1");
   const [web3Provider, setWeb3Provider] = useState(null);
   const [walletAddress, setWalletAddress] = useState(null);
   const [usdtBalance, setUsdtBalance] = useState(null);
-  const [usdtAmount, setUsdtAmount] = useState("");
+  const [usdtAmount, setUsdtAmount] = useState(null);
 
   const [transactionHash, setTransactionHash] = useState(null);
-  // const [contract1, setContract1] = useState<ethers.Contract | null>(null);
-  // const [contract2, setContract2] = useState<ethers.Contract | null>(null);
-  // const [contract3, setContract3] = useState<ethers.Contract | null>(null);
-  // const [contract4, setContract4] = useState<ethers.Contract | null>(null);
+  const [contract1, setContract1] = useState(null);
+  const [contract2, setContract2] = useState(null);
+  const [contract3, setContract3] = useState(null);
+  const [contract4, setContract4] = useState(null);
 
   function getSelectedRunner() {
     const runner = parseInt(selectedOption, 10);
@@ -639,17 +622,129 @@ export default function Trade() {
     return runner;
   }
 
-  const formatAddress = (address: string) => {
+  const formatAddress = (address) => {
     return `${address.slice(0, 4)}...${address.slice(-4)}`;
   };
-  async function fetchUsdtBalance(signer: any, address: any) {
+
+  async function connectWallet() {
+    try {
+      let web3Modal = new Web3Modal({
+        cacheProvider: false,
+        providerOptions,
+      });
+
+      const web3modalInstance = await web3Modal.connect();
+      const web3modalProvider = new ethers.providers.Web3Provider(
+        web3modalInstance
+      );
+      console.log(web3modalProvider);
+
+      if (web3modalProvider) {
+        const signer = web3modalProvider.getSigner();
+        const address = await signer.getAddress();
+        setWalletAddress(address);
+        setWeb3Provider(web3modalProvider);
+        const usdtContract = new ethers.Contract(usdtAddress, usdtABI, signer);
+        const balance = await usdtContract.balanceOf(address);
+        console.log(balance);
+        const decimals = 18;
+        setUsdtBalance(ethers.utils.formatUnits(balance, decimals));
+        await fetchUsdtBalance();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function fetchUsdtBalance(signer, address) {
     try {
       const usdtContract = new ethers.Contract(usdtAddress, usdtABI, signer);
       const balance = await usdtContract.balanceOf(address);
+      console.log(balance);
       const decimals = 18;
-      setUsdtBalance(ethers.utils.formatUnits(balance, decimals));
+      setUsdtBalance(ethers.utils.formatUnits(balance, 6));
     } catch (error) {
       console.error("Error fetching USDT balance:", error);
+    }
+  }
+
+  useEffect(() => {
+    if (walletAddress && web3Provider) {
+      const signer = web3Provider.getSigner();
+
+      const balanceInterval = setInterval(() => {
+        fetchUsdtBalance(signer, walletAddress);
+      }, 100000); // Fetch balance every 10 seconds
+
+      return () => clearInterval(balanceInterval);
+    }
+  }, [walletAddress, web3Provider]);
+  function convertTo18Decimals(usdtValue) {
+    const decimals = 18;
+    return ethers.utils.parseUnits(usdtValue, decimals);
+  }
+
+  async function approveUSDT() {
+    if (!web3Provider || !walletAddress) {
+      console.log("No provider or wallet connected");
+      return;
+    }
+
+    try {
+      const signer = web3Provider.getSigner();
+      const usdtContract = new ethers.Contract(usdtAddress, usdtABI, signer);
+
+      const amountToApprove = convertTo18Decimals(usdtAmount);
+
+      const tx = await usdtContract.approve(
+        preSaleContractAddress,
+        amountToApprove
+      );
+      console.log("Approval transaction hash:", tx.hash);
+      setTransactionHash(tx.hash); // Store transaction hash
+
+      // Wait for transaction to be confirmed
+      await tx.wait();
+      toast.success("USDT successfully approved!");
+    } catch (error) {
+      console.log("Error approving USDT:", error);
+      toast.error("Error approving USDT. Please try again.");
+    }
+  }
+
+  async function buyTokens() {
+    if (!web3Provider || !walletAddress) {
+      console.log("No provider or wallet connected");
+      return;
+    }
+
+    try {
+      const signer = web3Provider.getSigner();
+      const preSaleContract = new ethers.Contract(
+        preSaleContractAddress,
+        preSaleABI,
+        signer
+      );
+
+      // Define the values you want to send
+      const amountToBuy = convertTo18Decimals(usdtAmount); // Adjust to the correct amount of USDT
+      const buyerAddress = walletAddress;
+      const runner = selectedOption; // You can replace this with the actual value if needed
+      console.log(runner);
+      // // Estimate gas limit for the Buy transaction
+
+      // Call the Buy function
+      const tx = await preSaleContract.Buy(amountToBuy, walletAddress, runner, {
+        gasLimit: 10000000,
+      });
+      console.log(amountToBuy, buyerAddress, runner);
+
+      console.log("Transaction: ", tx);
+      // setTransactionHash(tx.hash);
+
+      toast.success("Tokens purchased successfully!");
+    } catch (error) {
+      console.log("Error purchasing tokens:", error);
+      toast.error("Error during token purchase. Please try again.");
     }
   }
 
@@ -661,13 +756,12 @@ export default function Trade() {
             href="https://flowbite.com/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"/> */}
             <Link href="/">
               <Image
                 src="/logo1.png"
                 width={10000}
                 height={10000}
-                alt="Picture of the author"
+                alt="Logo"
                 className="h-16 w-16"
               />
             </Link>
@@ -675,56 +769,26 @@ export default function Trade() {
               Gentop
             </span>
           </a>
-          <h1 className="text-white ml-5">
-              {usdtBalance !== null && <p>USDT Balance: {usdtBalance} USDT</p>}
-            </h1>
+          {/* <ToastContainer position="top-right" autoClose={50000} /> */}
+
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <Link
               href="/trade"
               className="text-white bg-[#14000b] border border-white hover:bg-transparent focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-8 py-2 text-center "
             >
-              <button className="" onClick={connectWallet}>
-                connect wallet
-              </button>
-              {/* {web3Provider == null ? (
+              {web3Provider == null ? (
                 <button className="" onClick={connectWallet}>
-
+                  connect Wallet{" "}
                 </button>
               ) : (
                 <div>
-                  <p>Connected</p>
+                  {/* <p>Connected</p> */}
+                  {walletAddress ? formatAddress(walletAddress) : ""}
                 </div>
-              )} */}
+              )}
             </Link>
-            <button
-              data-collapse-toggle="navbar-sticky"
-              type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-sticky"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
           </div>
-          <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-            id="navbar-sticky"
-          >
+          <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 text-white text-xl rounded-lg bg-[#14000b] md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
               <li>
                 <a href="/" className="block py-3 px-3 " aria-current="page">
@@ -754,14 +818,19 @@ export default function Trade() {
       <section>
         <div className="flex justify-center items-center min-h-screen">
           <div className="max-w-[720px] mx-auto">
+            {/* <h1 className="text-white"> Web3Modal connection</h1> */}
             <div className="text-white">
-              {/* {web3Provider == null ? ( */}
-
-              {/* ) : (
+              {" "}
+              {web3Provider == null ? (
+                <button className="" onClick={connectWallet}>
+                  connect Wallet{" "}
+                </button>
+              ) : (
                 <div>
-                  <p>Connected</p>
+                  {/* <p>Connected</p> */}
+                  <p>Address : {walletAddress}</p>
                 </div>
-              )} */}
+              )}
             </div>
             <div className="block mb-4 mx-auto border-b border-slate-300 pb-2 max-w-[360px]">
               <a
@@ -772,10 +841,11 @@ export default function Trade() {
                 Buy Your Own <b>Gentop Token</b>.
               </a>
             </div>
+            <h1 className="text-white ml-5">
+              {usdtBalance !== null && <p>USDT Balance: {usdtBalance} USDT</p>}
+            </h1>
             <div className="relative flex flex-col text-gray-700 bg-[#14000b] shadow-md w-96 rounded-xl bg-clip-border">
               <div className="max-w-md mx-auto bg-[#14000b] p-6 rounded-lg shadow-lg">
-                {/* <!-- Input for USDT --> */}
-
                 <label
                   htmlFor="usdt"
                   className="block text-sm font-medium text-white"
@@ -784,12 +854,11 @@ export default function Trade() {
                 </label>
                 <input
                   type="number"
-                  id="usdt"
+                  onChange={(e) => setUsdtAmount(e.target.value)}
                   className="mt-2 block  px-2 py-2 border text-[#14000b] w-96 border-gray-300 rounded-md focus:outline-none focus:ring-[#14000b] focus:border-[#14000b]"
                   placeholder="Enter amount in USDT"
                 />
 
-                {/* <!-- Dropdown for stacking options --> */}
                 <label
                   htmlFor="stacking"
                   className="mt-4 block text-sm font-medium text-white"
@@ -798,29 +867,43 @@ export default function Trade() {
                 </label>
                 <select
                   id="stacking"
-                  className="mt-2 block w-full px-3 py-2 border text-[#14000b] font-bold border-white rounded-md focus:outline-none focus:ring-[#14000b] focus:border-[#14000b]"
+                  value={selectedOption}
+                  className="mt-2 block w-full px-3 py-2 border text-center  text-[#14000b] font-bold border-white rounded-md focus:outline-none focus:ring-[#14000b] focus:border-[#14000b]"
+                  onChange={(e) => setSelectedOption(e.target.value)}
                 >
-                  <option value="bronze">Bronze - 120 days</option>
-                  <option value="silver">Silver - 90 days</option>
-                  <option value="gold">Gold - 45 days</option>
+                  Bronze <option value="1">Bronze</option>
+                  Silver <option value="2">Silver</option>
+                  Gold <option value="3">Gold</option>
                 </select>
 
-                {/* <!-- Approve Button --> */}
-                <button className="mt-6 w-full bg-white text-[#14000b] font-bold py-2 px-4 rounded-md hover:bg-[#14000b]focus:outline-none focus:ring-2 focus:ring-[#14000b] focus:ring-offset-2">
+                <button
+                  onClick={approveUSDT}
+                  className="mt-6 w-full bg-white text-[#14000b] font-bold py-2 px-4 rounded-md hover:bg-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
+                >
                   Approve
                 </button>
 
-                {/* <!-- Buy Button --> */}
-                <button className="mt-4 w-full  bg-white text-[#14000b] font-bold py-3 px-4 rounded-md hover:bg-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2">
+                <button
+                  onClick={buyTokens}
+                  className="mt-4 w-full bg-white text-[#14000b] font-bold py-3 px-4 rounded-md hover:bg-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
+                >
                   Buy
                 </button>
+                <div>
+                  {selectedOption === "1" && (
+                    <p className="text-white">120 Days Stacking</p>
+                  )}
+                  {selectedOption === "2" && (
+                    <p className="text-white">90 Days Stacking</p>
+                  )}
+                  {selectedOption === "3" && (
+                    <p className="text-white">45 Days Stacking</p>
+                  )}
+                </div>
               </div>
             </div>
           </div>
         </div>
-        {/* <div className="flex justify-center items-center min-h-screen">
-        <h1 className="text-7xl text-white"> Buy Page</h1>
-      </div> */}
       </section>
     </div>
   );
